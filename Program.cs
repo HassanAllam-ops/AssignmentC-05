4a362026fe2429c6c717ac6a79335e29a8b506de
Hassan Allam <hassan309pro@gmail.com>
2025-07-05 18:31:09 +0300

    Q4 : Create an Enum called "Colors" with the basic colors (Red, Green, Blue) as its members. Write a C# program that takes a color name as input from the user and displays a message indicating whether the input color is a primary color or not.

--- Program.cs
+++ Program.cs
@@ -24,89 +24,119 @@
     #endregion
 
     #region Enum Q3
-    [Flags]
-    enum Permissions : byte
-    {
-        Delete = 1,
-        Execute = 2,
-        Read = 4,
-        Write = 8,
-    }
+    //[Flags]
+    //enum Permissions : byte
+    //{
+    //    Delete = 1,
+    //    Execute = 2,
+    //    Read = 4,
+    //    Write = 8,
+    //}
     #endregion
     #region Class Q3
-    class User
+    //class User
+    //{
+    //    public int Id { get; set; }
+    //
+    //    public Permissions Permissions { get; set; }
+    #endregion
+
+    #region Enum Q4
+    enum Colors
     {
-        public int Id { get; set; }
+        Red,
+        Green,
+        Blue
+    }
+    #endregion
 
-        public Permissions Permissions { get; set; }
-        #endregion
 
 
-        internal class Program
-        {
+    internal class Program
+    {
             static void Main(string[] args)
             {
-                #region Q1
-                //Console.WriteLine("Days Of Week : ");
-                //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
-                //{
-                //    Console.WriteLine(day);
-                //}
-                #endregion
+            #region Q1
+            //Console.WriteLine("Days Of Week : ");
+            //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(day);
+            //}
+            #endregion
 
-                #region Q2
-                //Console.WriteLine("Enter Your Season (Spring , Summer , Autumn , Winter) : ");
-                //String Input = Console.ReadLine();
-                //if (Enum.TryParse(typeof(Season), Input , true  , out object result))
-                //{
-                //    Season season = (Season)result;
-                //
-                //    switch (season)
-                //    {
-                //        case Season.Spring:
-                //            Console.WriteLine("Spring : March to May");
-                //            break;
-                //        case Season.Summer:
-                //            Console.WriteLine("Summer : June to August");
-                //            break;
-                //        case Season.Autumn:
-                //            Console.WriteLine("Autmn : September to Novamber");
-                //            break;
-                //        case Season.Winter:
-                //            Console.WriteLine("Winter : December to February");
-                //            break;
-                //
-                //    }
-                //}
-                //else
-                //{
-                //    Console.WriteLine("Invalid season entered. Please enter Spring, Summer, Autumn, or Winter.");
-                //}
-                #endregion
+            #region Q2
+            //Console.WriteLine("Enter Your Season (Spring , Summer , Autumn , Winter) : ");
+            //String Input = Console.ReadLine();
+            //if (Enum.TryParse(typeof(Season), Input , true  , out object result))
+            //{
+            //    Season season = (Season)result;
+            //
+            //    switch (season)
+            //    {
+            //        case Season.Spring:
+            //            Console.WriteLine("Spring : March to May");
+            //            break;
+            //        case Season.Summer:
+            //            Console.WriteLine("Summer : June to August");
+            //            break;
+            //        case Season.Autumn:
+            //            Console.WriteLine("Autmn : September to Novamber");
+            //            break;
+            //        case Season.Winter:
+            //            Console.WriteLine("Winter : December to February");
+            //            break;
+            //
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid season entered. Please enter Spring, Summer, Autumn, or Winter.");
+            //}
+            #endregion
 
-                #region Q3
-                //User user01 = new User();
-                //user01.Id = 1;
-                //user01.Permissions = (Permissions)4;
-                //Console.WriteLine(user01.Permissions); // print Read
-                //// Add Permission to this variable
-                //user01.Permissions |= Permissions.Delete;  
-                //Console.WriteLine(user01.Permissions); // Add Delete Permission
-                //// Remove Permission from This Variable 
-                //user01.Permissions ^= Permissions.Read;
-                //Console.WriteLine(user01.Permissions); // Remove Read Permission
-                //// check if specific Permission existed inside variable
-                //if ((user01.Permissions & Permissions.Write) == Permissions.Write)
-                //{
-                //    Console.WriteLine("This Permission Is Exist Into This Variable");
-                //}
-                //else
-                //{
-                //    Console.WriteLine("This Permission Is not Exist Into This Variable");
-                //}
+            #region Q3
+            //User user01 = new User();
+            //user01.Id = 1;
+            //user01.Permissions = (Permissions)4;
+            //Console.WriteLine(user01.Permissions); // print Read
+            //// Add Permission to this variable
+            //user01.Permissions |= Permissions.Delete;  
+            //Console.WriteLine(user01.Permissions); // Add Delete Permission
+            //// Remove Permission from This Variable 
+            //user01.Permissions ^= Permissions.Read;
+            //Console.WriteLine(user01.Permissions); // Remove Read Permission
+            //// check if specific Permission existed inside variable
+            //if ((user01.Permissions & Permissions.Write) == Permissions.Write)
+            //{
+            //    Console.WriteLine("This Permission Is Exist Into This Variable");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This Permission Is not Exist Into This Variable");
+            //}
 
-                #endregion
+            #endregion
+
+            #region Q4
+            //Console.WriteLine("Enter The Color Name : ");
+            //string Input = Console.ReadLine();
+            //
+            //if (Enum.TryParse(typeof(Colors) , Input , true , out object result))
+            //{
+            //    Colors color = (Colors)result;
+            //
+            //    if (color == Colors.Red || color == Colors.Green || color == Colors.Blue)
+            //    {
+            //        Console.WriteLine($"{color} is a primary color");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{Input} is not a primary color");
+            //}
+            #endregion
+
             }
-        }
+       
     }
 }
