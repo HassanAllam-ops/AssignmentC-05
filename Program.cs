348c0189c691d19d64e23ac23aeced25ae282bc0
Hassan Allam <hassan309pro@gmail.com>
2025-07-05 15:18:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentC_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
